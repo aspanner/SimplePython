ebd9c8730d735373fddbfcab6aeff534b8f7fddd
aspanner <aspanner@redhat.com>
2015-11-27 14:36:39 +1100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "<h1> hello OpenShift Ninja 4</h1>"
+    return "<h1> hello OpenShift Ninja 5</h1>"
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
